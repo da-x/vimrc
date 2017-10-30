f5c18f6fc4396c63bb804a6ae678d92e83466363
Dan Aloni <alonid@gmail.com>
2017-10-30 10:55:56 +0200

    Adjust fixes.sh

--- fixes.sh
+++ fixes.sh
@@ -2,6 +2,8 @@
 
 # Here's a nice way to avoid commits to .gitignore for submodules.
 
-echo tags >> ./.git/modules/external/vim-textobj-dash/info/exclude
-echo tags >> ./.git/modules/external/vim-textobj-user/info/exclude
-echo tags >> ./.git/modules/external/vim-textobj-underscore/info/exclude
+G=$(git rev-parse --git-dir)
+
+echo tags >> ${G}/modules/external/vim-textobj-dash/info/exclude
+echo tags >> ${G}/modules/external/vim-textobj-user/info/exclude
+echo tags >> ${G}/modules/external/vim-textobj-underscore/info/exclude
