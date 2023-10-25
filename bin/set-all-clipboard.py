362f24d57f63bdef9f27586295b1a30d8c9b53ef
Dan Aloni <alonid@gmail.com>
2023-10-25 15:45:02 +0300

    Support ssh-cb for clipboard

--- bin/set-all-clipboard.py
+++ bin/set-all-clipboard.py
@@ -7,6 +7,18 @@ from threading import Thread
 
 inp = sys.stdin.read()
 
+def ssh_cb():
+    if os.getenv("SSH_CUSTOM_CALLBACK"):
+        p = os.popen("ssh-cb try-clipboard-set", "w")
+        p.write(inp)
+        res = p.close()
+        if res is None:
+            sys.exit(0)
+
+# Try ssh_cb first
+ssh_cb()
+
+# Otherwise, fallback
 def myfunc(cmd):
     p = os.popen("xsel -i -%s" % (i, ), "w")
     p.write(inp)
